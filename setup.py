cca1eab9d0c9c2674598583cffdac77edd0841db
drs251 <danrstephan@gmail.com>
2018-05-13 00:59:42 +0200

    Tweaked readme

--- setup.py
+++ setup.py
@@ -6,7 +6,17 @@ setup(
     packages=['pyRealParser'],
     url='https://github.com/drs251/pyRealParser',
     license='MIT',
+    platforms=['Windows', 'Mac OS X', 'Linux'],
     author='Daniel R. Stephan',
     author_email='danrstephan@gmail.com',
-    description='A Python package to read songs in the iRealPro fromat.'
+    keywords = ['music', 'chords', 'irealpro', 'jazz'],
+    description='A Python package to read songs in the iRealPro fromat.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+        "Topic :: Artistic Software",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Music"]
 )
